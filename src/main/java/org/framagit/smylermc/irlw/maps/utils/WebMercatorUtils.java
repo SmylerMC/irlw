51f6df38241b7317e654efa88a0e901e0f7abded
SmylerMC <smyler@mail.com>
2018-06-26 11:28:39 +0200

    Polished comments for WebMercatorUtils

--- src/main/java/org/framagit/smylermc/irlw/maps/utils/WebMercatorUtils.java
+++ src/main/java/org/framagit/smylermc/irlw/maps/utils/WebMercatorUtils.java
@@ -33,55 +33,61 @@ public class WebMercatorUtils {
 	
 	/**
 	 * 
-	 * @param lo The longitude in degrees
-	 * @param zoomlevel The zoom level
-	 * @return a double
+	 * @param longitude The longitude in degrees, between -180.0 and 180.0
+	 * @param zoomLevel The web-mercator zoom level, a positive integer
+	 * 
+	 * @return The X position corresponding to the given longitude
+	 * on a web-mercator map of the given zoom level, with 0;0 being the top left corner of the map.
 	 */
-	public static double getXFromLongitude(double lo, int zoomLevel){
-		return WebMercatorUtils.getXFromLongitudeRads(Math.toRadians(lo), zoomLevel);
+	public static double getXFromLongitude(double longitude, int zoomLevel){
+		return WebMercatorUtils.getXFromLongitudeRads(Math.toRadians(longitude), zoomLevel);
 	}
 	
 	
 	/**
 	 * 
-	 * @param lat The latitude in rads
-	 * @param zoomlevel The zoom level
-	 * @return a double
+	 * @param latitude The latitude in degrees, between -90.0 and 90.0
+	 * @param zoomlevel The web-mercator zoom level, a positive integer
+	 * 
+	 * @return The Y position corresponding to the given latitude
+	 * on a web-mercator map of the given zoom level, with 0;0 being the top left corner of the map.
 	 */
-	public static double getYFromLatitude(double lat, int zoomlevel){
-		return WebMercatorUtils.getYFromLatitudeRads(Math.toRadians(lat), zoomlevel);
+	public static double getYFromLatitude(double latitude, int zoomlevel){
+		return WebMercatorUtils.getYFromLatitudeRads(Math.toRadians(latitude), zoomlevel);
 	}
 	
 	
 	/**
 	 * 
-	 * @param lo The longitude in rads
-	 * @param zoomlevel The zoom level
-	 * @return a double
+	 * @param longitude The longitude in radians, between -pi and pi
+	 * @param zoomLevel The web-mercator zoom level, a positive integer
+	 * 
+	 * @return The X position corresponding to the given longitude
+	 * on a web-mercator map of the given zoom level, with 0;0 being the top left corner of the map.
 	 */
-	public static double getXFromLongitudeRads(double lo, int zoomlevel){
-		return ((double)(1 << (zoomlevel + 7))) * (lo + Math.PI) / Math.PI;
+	public static double getXFromLongitudeRads(double longitude, int zoomlevel){
+		return ((double)(1 << (zoomlevel + 7))) * (longitude + Math.PI) / Math.PI;
 	}
 	
 	
 	/**
 	 * 
-	 * @param lat The latitude in rads
-	 * @param zoomlevel The zoom level
-	 * @return a double
+	 * @param latitude The latitude in radians, between -pi/2 and pi/2
+	 * @param zoomlevel The web-mercator zoom level, a positive integer
+	 * 
+	 * @return The Y position corresponding to the given latitude
+	 * on a web-mercator map of the given zoom level, with 0;0 being the top left corner of the map.
 	 */
 	public static double getYFromLatitudeRads(double lat, int zoomlevel){
 		return (128f/Math.PI) * (double)(1 << zoomlevel) * (Math.PI - Math.log(Math.tan( Math.PI / 4  + lat /2)));
 	}
 	
 	
-	/**
-	 * Returns the latitude of a given y point of the map
+	/** 
+	 * @param y a coordinate, as a positive integer, with 0;0 as the top left corner
+	 * @param zoomLevel The web-mercator zoom level, a positive integer
 	 * 
-	 * @param y
-	 * @param zoomLevel
-	 * 
-	 * @return The latitude in degrees
+	 * @return The corresponding latitude in degrees, between -180.0 and 180.0
 	 */
 	public static double getLatFromY(double y, int zoomLevel){
 		return Math.toDegrees(WebMercatorUtils.getLatFromYRads(y, zoomLevel));
@@ -89,40 +95,32 @@ public class WebMercatorUtils {
 	
 	
 	/**
-	 * Returns the longitude of a given x point of the map
+	 * @param x a coordinate, as a positive integer, with 0;0 as the top left corner
+	 * @param zoomLevel The web-mercator zoom level, a positive integer
 	 * 
-	 * @param y
-	 * @param zoomLevel
-	 * 
-	 * @return The longitude in degrees
+	 * @return The corresponding longitude in degrees, between -90.0 and 90.0
 	 */
 	public static double getLongFromX(double x, int zoomLevel){
 		return Math.toDegrees(WebMercatorUtils.getLongFromXRads(x, zoomLevel));
 	}
 	
 	
-	/**
-	 * Returns the latitude of a given y point of the map
+	/** 
+	 * @param y a coordinate, as a positive integer, with 0;0 as the top left corner
+	 * @param zoomLevel The web-mercator zoom level, a positive integer
 	 * 
-	 * @param y
-	 * @param zoomLevel
-	 * 
-	 * @return The latitude in radians
+	 * @return The corresponding latitude in radians, between -pi and pi
 	 */
 	public static double getLatFromYRads(double y, int zoomLevel){
-		return 2 * (Math.atan(Math.pow(Math.E, - ((y * Math.PI) / (1 << zoomLevel + 7) - Math.PI))) - Math.PI / 4);
-		
-		
+		return 2 * (Math.atan(Math.pow(Math.E, - ((y * Math.PI) / (1 << zoomLevel + 7) - Math.PI))) - Math.PI / 4);		
 	}
 	
 	
 	/**
-	 * Returns the longitude of a given x point of the map
+	 * @param x a coordinate, as a positive integer, with 0;0 as the top left corner
+	 * @param zoomLevel The web-mercator zoom level, a positive integer
 	 * 
-	 * @param y
-	 * @param zoomLevel
-	 * 
-	 * @return The longitude in radians
+	 * @return The corresponding longitude in radians, between -pi/2 and pi/2
 	 */
 	public static double getLongFromXRads(double x, int zoomLevel){
 		return (Math.PI * x) / (1 << 7 + zoomLevel) - Math.PI;
